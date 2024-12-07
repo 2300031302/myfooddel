5e071589bad284e9281ef1b380b9b5efe0454708
2300031302_vijayaraju <162673844+2300031302@users.noreply.github.com>
2024-12-07 22:08:20 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className='header-contents'>
                 <h2>Stay cheesy and keep eating delicious food.</h2>
                 <p>Explore a menu of expertly crafted dishes made with the finest ingredients. Satisfy your cravings and elevate your dining experience with every bite.</p>
-                <button>View Menu</button>
+                
             </div>
         </div>
     )
